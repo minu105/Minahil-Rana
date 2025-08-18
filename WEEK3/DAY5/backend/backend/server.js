d09bc76528ff05dbc864c3c38f57e642fe29558b
minu105 <minahil_intern@netixsol.com>
2025-08-18 12:14:21 +0500

    deployment fix

--- WEEK3/DAY5/backend/backend/server.js
+++ WEEK3/DAY5/backend/backend/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const dotenv = require("dotenv")
 const swaggerUi = require("swagger-ui-express")
+const path = require("path")
 
 // Import configuration and documentation
 const connectDB = require("./src/config/db")
