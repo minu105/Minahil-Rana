4fe78c2941643cd8caa1991f97a55fbee5f4a7ea
minu105 <minahil_intern@netixsol.com>
2025-08-23 15:58:43 +0500

    deployment fixes

--- WEEK4/Day5/backend/server.js
+++ WEEK4/Day5/backend/server.js
@@ -24,7 +24,7 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 // Middleware
 app.use(
   cors({
-    origin: process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(",") : ["https://minahil-rana.vercel.app"],
+    origin: process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(",") : ["https://minahilshabbir-week4-day5-hackathon.vercel.app"],
     credentials: true,
   }),
 )
